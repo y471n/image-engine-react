c0a44289cce9fd17eb242f695714a26d290046c3
Yatin <iamyatin@gmail.com>
2021-06-07 08:10:30 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, Typography } from "@material-ui/core";
+import React from "react";
+import "./App.css";
+import MasonryList from "./components/MasonryList";
+import tiles from "./data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Box m={3}>
+          <Typography variant="h2" component="h1">
+            Getting Started with Image Engine in React
+          </Typography>
+        </Box>
+        <section>
+          <MasonryList tiles={tiles} />
+        </section>
+        <footer></footer>
+      </Container>
     </div>
   );
 }
---
+++ src/data.tsx
@@ -0,0 +1,76 @@
+const tiles = [
+  {
+    img: "breakfast.jpeg",
+    title: "breakfast",
+    author: "author",
+    cols: 2,
+  },
+  {
+    img: "burgers.jpeg",
+    title: "burgers",
+    author: "author",
+    cols: 1,
+  },
+  {
+    img: "camera.jpeg",
+    title: "camera",
+    author: "author",
+    cols: 1,
+  },
+  {
+    img: "morning.jpeg",
+    title: "morning",
+    author: "author",
+    cols: 1,
+  },
+  {
+    img: "hats.jpeg",
+    title: "hats",
+    author: "author",
+    cols: 1,
+  },
+  {
+    img: "honey.jpeg",
+    title: "honey",
+    author: "author",
+    cols: 1,
+  },
+  {
+    img: "vegetables.jpeg",
+    title: "vegetables",
+    author: "author",
+    cols: 2,
+  },
+  {
+    img: "plant.jpeg",
+    title: "plant",
+    author: "author",
+    cols: 1,
+  },
+  {
+    img: "mushroom.jpeg",
+    title: "mushroom",
+    author: "author",
+    cols: 1,
+  },
+  {
+    img: "olive.jpeg",
+    title: "olive",
+    author: "author",
+    cols: 1,
+  },
+  {
+    img: "star.jpeg",
+    title: "star",
+    author: "author",
+    cols: 2,
+  },
+  {
+    img: "bike.jpeg",
+    title: "bike",
+    author: "author",
+    cols: 1,
+  },
+];
+
+export default tiles;
